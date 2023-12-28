e44389e6187179bebc886882b1b021968485e82e
Facundo Seib <153626798+fseib@users.noreply.github.com>
2023-12-28 11:33:56 -0300

    Devolucion de codigo de TabsScreen

--- ticketera-website/src/components/root/TabsScreen.js
+++ ticketera-website/src/components/root/TabsScreen.js
@@ -19,5 +19,24 @@ export const TabsScreen = () => {
   };
 
   return (
-   <div style={{backgroundColor: 'red'}}>ddd</div>)
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Stack direction="row" spacing={2}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Item One 1" value="1" />
+            <Tab label="Item Two 2" value="2" />
+{/*             <Tab label="Item Three 3" value="3" /> */}
+          </TabList>
+          <IconButton color="primary" aria-label="add an alarm">
+        <QueueIcon />
+      </IconButton>
+          </Stack>
+        </Box>
+        <TabPanel value="1"> <TabItem /> </TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+       {/*  <TabPanel value="3">Item Three</TabPanel> */}
+      </TabContext>
+    </Box>
+  );
 }
