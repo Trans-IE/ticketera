98e290add8845468f40b6e583fb059be28011cde
spalacio82 <44677003+trans-iesa@users.noreply.github.com>
2024-01-09 12:13:18 -0300

    nuevos entities brands

--- ticketera-website/src/redux/actions/entitiesActions.js
+++ ticketera-website/src/redux/actions/entitiesActions.js
@@ -1,10 +1,11 @@
 import { fetchConToken, fetchSinToken } from '../../helpers/fetch';
 import { getConfigData, getURLFromConfigByName } from '../../helpers/getConfigFunctions';
 import encryptStorage from '../../helpers/storageEncrypter';
-import { brandsGetRowsetRedux } from '../slices/entitiesSlice';
+import { entitiesClearDataRedux, brandGetRowsetRedux, brandCreateRedux, brandUpdateRedux } from '../slices/entitiesSlice';
 
 
 export const brandGetRowset = () => {
+
     return async(dispatch, getState ) => {
 
         try {
@@ -15,7 +16,7 @@ export const brandGetRowset = () => {
             const body = await resp.json();
             if( body.ok ) 
             {
-                dispatch( brandsGetRowsetRedux( body.value ) );
+                dispatch( brandGetRowsetRedux( body.value ) );
             }
             else 
             {
@@ -25,6 +26,68 @@ export const brandGetRowset = () => {
         } catch (error) {
             console.log(error);
             console.log("ERROR brandGetRowset SIN BODY");
+            throw new Error( error.message );
+        }
+
+    }
+}
+
+export const brandUpdate = ( brand ) => {
+    return async(dispatch, getState ) => {
+
+        try {
+            const { config } = getState().auth;
+            let url = getURLFromConfigByName(config, "endpoint_host", `entities/updateBrand/${ brand.id }`);
+            const resp = await fetchConToken(url, vdn, 'PUT' );
+            const body = await resp.json();
+
+            if ( body.ok ) {
+            //    dispatch( vdnUpdatedOK( vdn ) );
+                dispatch (brandUpdateRedux(brand));
+                return body.msg;
+            } else {
+                console.log("FALSE BODY ");
+                if (body.hasOwnProperty("error")) {
+                    throw new Error( body.error.message);
+                } else {
+                    throw new Error( body.msg) ;
+                }
+            }
+
+        } catch (error) {
+            console.log(error);
+            console.log("ERROR brandUpdate SIN BODY");
+            throw new Error( error.message );
+        }
+
+    }
+}
+
+export const brandDelete = ( brand ) => {
+    return async(dispatch, getState ) => {
+
+        try {
+            const { config } = getState().auth;
+            let url = getURLFromConfigByName(config, "endpoint_host", `entities/deleteBrand/${ brand.id }`);
+            const resp = await fetchConToken(url, vdn, 'DELETE' );
+            const body = await resp.json();
+
+            if ( body.ok ) {
+            //    dispatch( vdnUpdatedOK( vdn ) );
+                dispatch (brandDelete(brand));
+                return body.msg;
+            } else {
+                console.log("FALSE BODY ");
+                if (body.hasOwnProperty("error")) {
+                    throw new Error( body.error.message);
+                } else {
+                    throw new Error( body.msg) ;
+                }
+            }
+
+        } catch (error) {
+            console.log(error);
+            console.log("ERROR brandDelete SIN BODY");
             throw new Error( error.message );
         }
 
