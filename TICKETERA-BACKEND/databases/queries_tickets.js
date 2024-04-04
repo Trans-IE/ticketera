fe6188e021ee8e43fbfe26cf8ff8a4e0cd648d75
gperdomoTrans <gperdomo@trans.com.ar>
2024-04-04 12:37:49 -0300

    Se actualizó versión de f_search_getticketsdataset a v1

--- TICKETERA-BACKEND/databases/queries_tickets.js
+++ TICKETERA-BACKEND/databases/queries_tickets.js
@@ -84,7 +84,7 @@ const deleteDBTicket = (id) => {
 
 const getAllDBTicketsByFilter = (titulo, causaRaiz, ticketPartner, empresaId, productoId, responsableId, numeroId, prioridad, estado, tipoFalla, dateFrom, dateTo, tipoUsuario, usuarioId, offset, tipoTicket, tksinac, orderBy, orderByType, limit) => {
     const return_promise = new Promise((resolve, reject) => {
-        pooldata.getPool.query('select * from f_search_getticketsdataset_v5($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20);', [titulo, causaRaiz, ticketPartner, empresaId, productoId, responsableId, numeroId, prioridad, estado, tipoFalla, dateFrom, dateTo, tipoUsuario, usuarioId, offset, tipoTicket, tksinac, orderBy, orderByType, limit], (error, results) => {
+        pooldata.getPool.query('select * from f_search_getticketsdataset_v1($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20);', [titulo, causaRaiz, ticketPartner, empresaId, productoId, responsableId, numeroId, prioridad, estado, tipoFalla, dateFrom, dateTo, tipoUsuario, usuarioId, offset, tipoTicket, tksinac, orderBy, orderByType, limit], (error, results) => {
             if (error) {
                 reject(error.message);
             }
