b70568806a692484b12a267dc3e7097f9029b2d9
Facundo Seib <153626798+fseib@users.noreply.github.com>
2024-07-05 17:14:36 -0300

    Cambio nuevo test pull request

--- ticketera-website/src/components/ui/ToogleButtonCtrl.js
+++ ticketera-website/src/components/ui/ToogleButtonCtrl.js
@@ -14,7 +14,7 @@ const ToogleButtonStyled = styled(ToggleButton)`
 `;
 
  */
-//TEST PULL REQUEST   aa
+//TEST PULL REQUEST 2 Ticketera
 
 const ToogleButtonStyled = styled(ToggleButton)(({ theme, borderRadius, borderColor, colorHover, colorSelected, colorBackground, borderSize, padding }) => ({
   //  color: theme.palette.primary.main,
