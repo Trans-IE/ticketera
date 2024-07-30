749b696bad9d77a238fbfb960f8a9df22d47d107
spalacio82 <44677003+trans-iesa@users.noreply.github.com>
2024-07-30 11:56:34 -0300

    presentacion de archivos cargados

--- ticketera-website/src/components/ticket/ShowFilesModal/ShowFilesModal.js
+++ ticketera-website/src/components/ticket/ShowFilesModal/ShowFilesModal.js
@@ -1,5 +1,6 @@
 import { Box, Button, IconButton, ListItem, ListItemButton, ListItemText, Modal, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import React, { useEffect, useState } from 'react'
+import { grey } from '@mui/material/colors';
 import { ButtonTrans } from '../../ui/ButtonTrans';
 import { useDispatch } from 'react-redux';
 import { toast } from 'sonner';
@@ -31,17 +32,14 @@ export const ShowFilesModal = ({ ticketId, closeModal }) => {
     const [rowsPerPageFiles, setRowsPerPageFiles] = useState(10);
     const [actualOffsetFiles, setActualOffsetFiles] = useState(0);
     const [resetPaginationFiles, setResetPaginationFiles] = useState(false);
+    const [filesList, setfilesList] = useState([]);
+    const [hasMorePages, setHasMorePages] = useState(true);
 
     const columnsData = [
-        { id: 'priority', label: '', cellWidth: 0, visible: true },
-        { id: 'type', label: '', cellWidth: 0, visible: true },
-        { id: 'id', label: 'ID', cellWidth: 0, visible: true },
-        { id: 'titulo', label: 'Titulo', cellWidth: 0, visible: true, backgroundColor: "#b5b3b3", color: 'black' },
-        { id: 'empresa', label: 'Empresa', cellWidth: 0, visible: true, backgroundColor: "#b5b3b3", color: 'black' },
-        { id: 'tipo_falla', label: 'Tipo de falla', cellWidth: 0, visible: true, backgroundColor: "#b5b3b3", color: 'black' },
-        { id: 'responsable', label: 'Responsable', cellWidth: 0, visible: true, backgroundColor: "#b5b3b3", color: 'black' },
-        { id: 'estado', label: 'Estado', cellWidth: 0, visible: true, backgroundColor: "#b5b3b3", color: 'black' },
-        { id: 'fecha_creacion', label: 'Creado', cellWidth: 0, visible: true, backgroundColor: "#b5b3b3", color: 'black' }
+        { id: 'archivo', label: 'Archivo', cellWidth: 1, visible: true, backgroundColor: "#b5b3b3", color: 'black' },
+        { id: 'fecha', label: 'Fecha', cellWidth: 1, visible: true, backgroundColor: "#b5b3b3", color: 'black' },
+        { id: 'usuarios_nombre', label: 'Nombre', cellWidth: 1, visible: true, backgroundColor: "#b5b3b3", color: 'black' },
+        { id: 'usuarios_apellido', label: 'Apellido', cellWidth: 1, visible: true, backgroundColor: "#b5b3b3", color: 'black' }
     ]
 
     const handleGridChangePageFiles = (newpage_limit, newpage_offset) => {
@@ -58,7 +56,18 @@ export const ShowFilesModal = ({ ticketId, closeModal }) => {
 
         // getAllFilesPaths
 
-        dispatch(getAllFilesByTicketId(ticketId)).then((listOfFiles) => {
+        dispatch(getAllFilesByTicketId(ticketId, 0, 15)).then((listOfFiles) => {
+
+            console.log("lista de archivos obtenidos 3 : ", listOfFiles);
+
+            if (listOfFiles.length < 15) {
+                setHasMorePages(false)
+            }
+            else {
+                setHasMorePages(true)
+            }
+
+            setfilesList(listOfFiles);
 
         })
 
@@ -77,47 +86,51 @@ export const ShowFilesModal = ({ ticketId, closeModal }) => {
                         <h2>Detalle de archivos cargados al ticket N°</h2>
 
                     </div>
-                    {
-                        [].length > 0 && (
-                            <GridViewBigData
-                                columns={columnsData}
-                                data={[]}
-                                customButtonNumber={0}
-                                customButtonTooltip={[]}
-                                customButtonIcon={[]}
-                                handleCustomButton={[]}
-                                customButtonEnable={[false]}
-                                initRowsPerPage={rowsPerPageFiles}
-                                handleGridChangePage={handleGridChangePageFiles}
-                                resetPagination={resetPaginationFiles}
-                                gridDataHasMorePages={hasMorePages}
-                                showColumnSelector={false}
-                                gridSelectionOnClick={GridSelectionOnClickHandleSelectFile}
-                                detailcolumns={[]}
-                                oneExpandOnly={false}
-                                specialButtonStyle={{ backgroundColor: "#b5b3b3", color: 'black' }}
-                                handleOnExpand={(item, expand) => {
-
-                                }}
-                                canReorderColumns={true}
-                                customButtonNumberDetail={0}
-                                customButtonEnableDetail={[]}
-                                customButtonTooltipDetail={[]}
-                                customButtonIconDetail={[]}
-                                handleCustomButtonDetail={[]}
-
-                                subDataActionHeaderStyle={{ backgroundColor: grey[400], color: 'black', zIndex: 1 }}
-                                subDataActionRowsStyle={{ backgroundColor: grey[50] }}
-                                subDataActionColumnShowLeft={true}
-                                maxHeight={'calc(100vh - 155px)'}
-                            />
-                        )
-                    }
-
-                    <div style={{ marginTop: '10px', height: '5%', display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+                    <div style={{ marginTop: '10px', marginBottom: '50px', height: '5%', display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
                         <ButtonTrans onClick={() => closeModal()} variant='outlined' marginLeft>Cargar Archivos</ButtonTrans>
                         <ButtonTrans onClick={() => closeModal()} variant='outlined' marginLeft>Cerrar</ButtonTrans>
                     </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+
+
+                        {
+                            filesList.length > 0 && (
+                                <GridViewBigData
+                                    columns={columnsData}
+                                    data={filesList}
+                                    customButtonNumber={0}
+                                    customButtonTooltip={[]}
+                                    customButtonIcon={[]}
+                                    handleCustomButton={[]}
+                                    customButtonEnable={[false]}
+                                    initRowsPerPage={rowsPerPageFiles}
+                                    handleGridChangePage={handleGridChangePageFiles}
+                                    resetPagination={resetPaginationFiles}
+                                    gridDataHasMorePages={hasMorePages}
+                                    showColumnSelector={false}
+                                    gridSelectionOnClick={GridSelectionOnClickHandleSelectFile}
+                                    detailcolumns={[]}
+                                    oneExpandOnly={false}
+                                    specialButtonStyle={{ backgroundColor: "#b5b3b3", color: 'black' }}
+                                    handleOnExpand={(item, expand) => {
+
+                                    }}
+                                    canReorderColumns={true}
+                                    customButtonNumberDetail={0}
+                                    customButtonEnableDetail={[]}
+                                    customButtonTooltipDetail={[]}
+                                    customButtonIconDetail={[]}
+                                    handleCustomButtonDetail={[]}
+
+                                    subDataActionHeaderStyle={{ backgroundColor: grey[400], color: 'black', zIndex: 1 }}
+                                    subDataActionRowsStyle={{ backgroundColor: grey[50] }}
+                                    subDataActionColumnShowLeft={true}
+                                    maxHeight={'calc(100vh - 155px)'}
+                                />
+                            )
+                        }
+                    </div>
+
                 </div>
 
             </div>
--- ticketera-website/src/redux/actions/ticketActions.js
+++ ticketera-website/src/redux/actions/ticketActions.js
@@ -371,17 +371,18 @@ export const uploadFiles = (files, ticketId) => {
     }
 }
 
-export const getAllFilesByTicketId = ({ ticketId, list_offset, list_limit }) => {
+export const getAllFilesByTicketId = (ticketId, list_offset, list_limit) => {
     return async (dispatch, getState) => {
         try {
             //getAllFilesPaths
             const { config } = getState().auth;
             let url = getURLFromConfigByName(config, "api_gateway_host", "entities/getAllFilesPaths");
+            //    console.log(`llamando a getAllFilesPaths ticket_id: ${ticketId} offset: ${list_offset} limit: ${list_limit}  `);
             const resp = await fetchConToken(url, { ticket_id: ticketId, offset: list_offset, limit: list_limit }, 'POST');
             const body = await resp.json();
             if (body.ok) {
-                console.log("lista de archivos: ", body.value);
-                return body.value
+                //        console.log("lista de archivos 1 : ", body.value);
+                return body.value;
             } else {
                 Swal.fire('Error', body.msg, 'error');
             }
