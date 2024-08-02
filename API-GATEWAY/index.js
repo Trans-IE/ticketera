7b0e0d62809e281c1e77c71c4cb8baa1f1da21bd
Mmaruzza <mmaruzza@trans.com.ar>
2024-08-02 18:32:54 -0300

    Backend n Notifications dockerized

--- API-GATEWAY/index.js
+++ API-GATEWAY/index.js
@@ -1,50 +1,75 @@
-const express = require('express');
-const cors = require('cors');
-const fs = require('node:fs');
-const https = require('node:https');
+const express = require("express");
+const cors = require("cors");
+const fs = require("node:fs");
+const https = require("node:https");
+const path = require("node:path");
+
+// Crear el directorio de logs si no existe
+const logDir = path.join(__dirname, "logs");
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir, { recursive: true });
+  fs.chmodSync(logDir, 0o777);
+}
+
 // Crear el servidor de express
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./swagger');
-const { checkAndCreateFolder, deleteTmpFiles } = require('./helpers/fileHelper');
-const path = require('node:path');
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
+const {
+  checkAndCreateFolder,
+  deleteTmpFiles,
+} = require("./helpers/fileHelper");
 
 // CORS
-app.use(cors())
+app.use(cors());
 
 // Directorio Público
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Lectura y parseo del body
-app.use(express.json({ limit: '100mb' }));
+app.use(express.json({ limit: "100mb" }));
 
 // Routers
 // authentication
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/entities/', require('./routes/entities'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/entities/", require("./routes/entities"));
 
-if (process.env.SHOW_DOCUMENTATION === 'ON') {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+if (process.env.SHOW_DOCUMENTATION === "ON") {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
 
 const checkDeleteTmpFiles = () => {
-    deleteTmpFiles(`${__dirname}${path.sep}uploads`, 3600)
-}
+  deleteTmpFiles(`${__dirname}${path.sep}uploads`, 3600);
+};
 
-checkAndCreateFolder(`${__dirname}${path.sep}uploads`)
+checkAndCreateFolder(`${__dirname}${path.sep}uploads`);
 
-if (process.env.SSL === 'ON') {
-    const privateKey = fs.readFileSync(`sslcert/${process.env.SSL_KEY_CRT_NAME}.key`, 'utf8');
-    const certificate = fs.readFileSync(`sslcert/${process.env.SSL_KEY_CRT_NAME}.crt`, 'utf8');
-    const credentials = { key: privateKey, cert: certificate };
-    var httpsServer = https.createServer(credentials, app);
-    httpsServer.listen(process.env.PORT, () => {
-        console.log(`API-GATEWAY is working in port ${process.env.PORT}. SSL: ON`);
-        setInterval(checkDeleteTmpFiles, 3600000);
-    });
+if (process.env.SSL === "ON") {
+  const privateKey = fs.readFileSync(
+    `sslcert/${process.env.SSL_KEY_CRT_NAME}.key`,
+    "utf8"
+  );
+  const certificate = fs.readFileSync(
+    `sslcert/${process.env.SSL_KEY_CRT_NAME}.crt`,
+    "utf8"
+  );
+  const credentials = { key: privateKey, cert: certificate };
+  var httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(process.env.PORT, () => {
+    console.log(`API-GATEWAY is working in port ${process.env.PORT}. SSL: ON`);
+    setInterval(checkDeleteTmpFiles, 3600000);
+  });
 } else {
-    app.listen(process.env.PORT, () => {
-        console.log(`API-GATEWAY is working in port ${process.env.PORT}. SSL: OFF`);
-        setInterval(checkDeleteTmpFiles, 3600000);
-    });
+  app.listen(process.env.PORT, () => {
+    console.log(`API-GATEWAY is working in port ${process.env.PORT}. SSL: OFF`);
+    setInterval(checkDeleteTmpFiles, 3600000);
+  });
 }
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
+});
